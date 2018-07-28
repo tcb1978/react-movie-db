6198569281feac3197199c2e86b3f713e9821f6e
Matthew Eldredge <mattheweldredge@gmail.com>
2018-07-27 20:27:26 -0700

    deconstruct props on welcome tag

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Welcome text="This is the text prop" />
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+class Welcome extends Component {
+  render() {
+    const { text } = this.props;
+    return (
+      <h1 className="App-title">{text}</h1>
+    )
+  }
+}
+
+export default App;

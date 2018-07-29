a4592dc862cc652e9732ec6557debb9e64305c6b
Matthew Eldredge <mattheweldredge@gmail.com>
2018-07-28 21:51:08 -0700

    change movie to stateless functional component

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ import logo from './logo.svg';
 import Movie from './Movie';
 
 class App extends Component {
-  
   state = {
-    movies:[]
+    movies: [],
   }
 
   async componentDidMount() {
@@ -14,8 +13,8 @@ class App extends Component {
       const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=58cb30722b0c1eb592fd297cfa637c39&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
       const movies = await res.json();
       this.setState({
-        movies: movies.results
-      })
+        movies: movies.results,
+      });
     } catch (error) {
       console.log(error);
     }
@@ -26,12 +25,11 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
+          <img src={logo} className="App-logo" alt="logo" />
         </header>
-        {movies.map( movie => <Movie key={movie.id} movie={movie} /> )}
+        {movies.map(movie => <Movie key={movie.id} movie={movie} />)}
       </div>
     );
   }
 }
-
 export default App;
